5fb8d0cf0a568731c7d5e58e62c744588383032b
Joseph <Joseph_Zhuo@syscom.com.tw>
2023-08-07 09:07:29 +0800

    修改mapper注入方法

--- go/demo/src/main/java/com/springboot/demo/impl/UserImpl.java
+++ go/demo/src/main/java/com/springboot/demo/impl/UserImpl.java
@@ -3,16 +3,17 @@ package com.springboot.demo.impl;
 import com.springboot.demo.mapper.UserMapper;
 import com.springboot.demo.model.User;
 import com.springboot.demo.service.UserService;
+import com.springboot.demo.util.SpringBeanFactoryUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 //@Service
 @Component
 public class UserImpl implements UserService{
-    @Autowired(required = false)
-    UserMapper usermapper;
+//    @Autowired(required = false)
+//    UserMapper usermapper;
 
-//    private userMapper usermapper;
+    private UserMapper usermapper = SpringBeanFactoryUtil.getBean(UserMapper.class);
     public User addMember(String username, String password){
         return usermapper.findByUserName(username,password);
     }
