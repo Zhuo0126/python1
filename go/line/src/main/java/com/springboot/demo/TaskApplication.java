1340192e0b2535b63c20b8c906a2951ebf214e04
Joseph <Joseph_Zhuo@syscom.com.tw>
2023-11-09 14:59:56 +0800

    新增一個threads處理多筆數據的模組

---
+++ go/line/src/main/java/com/springboot/demo/TaskApplication.java
@@ -0,0 +1,34 @@
+package com.springboot.demo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@SpringBootApplication
+public class TaskApplication {
+
+	public static void main(String[] args) {
+		int numberOfTransactions = 1000;
+		ExecutorService executor = Executors.newFixedThreadPool(10); //10個線程
+
+		for (int i = 0; i < numberOfTransactions; i++) {
+			Runnable transactionTask = new TransactionTask(i);
+			executor.execute(transactionTask);
+		}
+
+		executor.shutdown();
+	}
+	static class TransactionTask implements Runnable {
+		private int transactionId;
+
+		public TransactionTask(int transactionId) {
+			this.transactionId = transactionId;
+		}
+
+		@Override
+		public void run() {
+			System.out.println(transactionId + " on thread: " + Thread.currentThread().getName());
+		}
+	}
+}
